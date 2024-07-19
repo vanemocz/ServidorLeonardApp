903b1aa9f04a11b1e1d6b08c05769dfaf3d2a15a
santigo-94 <santigo94@hotmail.es>
2024-07-19 17:22:27 -0500

    actualizacion clase viernes

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
+
 from routes.rutasAPI import rutas
 
 app=FastAPI()
+
+# Habilitar CORS para permitir solicitudes desde todos los orígenes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite solicitudes desde cualquier origen. Ajusta según sea necesario.
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP. Ajusta según sea necesario.
+    allow_headers=["*"],  # Permite todas las cabeceras. Ajusta según sea necesario.
+)
 
 app.include_router(rutas)
--- database/basedatos.py
+++ database/basedatos.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,MetaData
 
-motorBD=create_engine("mysql+pymysql://root@localhost:3306")
+motorBD=create_engine("mysql+pymysql://root@localhost:3306/leonardappdb")
 
 metaDatos=MetaData()
 
